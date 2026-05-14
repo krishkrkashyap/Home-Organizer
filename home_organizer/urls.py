6b58c26a3c17e6b17e1e65491fe08922be0a8990
Krish Kumar <Krishkrkashyap@gmail.com>
2026-05-14 10:17:24 +0530

    feat: add base template and dashboard with stats cards

--- home_organizer/urls.py
+++ home_organizer/urls.py
@@ -1,22 +1,24 @@
-"""
-URL configuration for home_organizer project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import render
+
+
+def dashboard(request):
+    from staff.models import StaffProfile
+    from kitchen.models import PantryItem
+    from tasks_app.models import AssignedTask, Reminder
+    return render(request, 'dashboard.html', {
+        'staff_count': StaffProfile.objects.filter(is_active=True).count(),
+        'pantry_count': PantryItem.objects.count(),
+        'pending_tasks': AssignedTask.objects.filter(is_completed=False).count(),
+        'upcoming_reminders': Reminder.objects.filter(is_completed=False).count(),
+    })
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', dashboard, name='dashboard'),
+    path('staff/', include('staff.urls')),
+    path('kitchen/', include('kitchen.urls')),
+    path('tasks/', include('tasks_app.urls')),
 ]
